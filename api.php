a701c3ca5ed3270fd8571ed64e32293085d8c5bf
Milos <webad012@gmail.com>
2022-08-17 20:52:52 +0200

    updated api to cover various request post types

--- api.php
+++ api.php
@@ -1,11 +1,32 @@
 <?php
 
+header('Content-Type: application/json; charset=utf-8');
+
 try
 {
     $url = filter_input(INPUT_POST, 'url');
+    // $url = filter_input(INPUT_GET, 'url');
+
     if(empty($url))
     {
-        exit('empty url param');
+        if( empty($_POST) )
+        {
+            $php_input_obj = json_decode(file_get_contents('php://input'));
+            if(property_exists($php_input_obj, 'url'))
+            {
+                $url = $php_input_obj->url;
+            }
+        }
+        else if(isset($_POST['url']))
+        {
+            $url = $_POST['url'];
+        }
+    }
+
+    if(empty($url))
+    {
+        echo json_encode(['error' => 'empty url param']);
+        exit();
     }
 
     require_once(__DIR__.'/SUFPURS.php');
@@ -18,5 +39,7 @@ try
 }
 catch(\Exception $e)
 {
-    exit($e->getMessage());
+    echo json_encode(['error' => $e->getMessage()]);
 }
+
+exit();
